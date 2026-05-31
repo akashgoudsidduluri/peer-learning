570825e74aeda4bf6c182c17d6ce2513ca1ef109
ArshVermaGit <arshverma.dev@gmail.com>
2026-06-01 02:35:40 +0530

    fix: Add missing CORS configuration to Express backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { randomUUID } from "crypto";
+import cors from "cors";
 import authRoutes from "./routers/authRoutes.js";
 import chatRoutes from "./routers/chatRoutes.js";
 import aiRoutes from "./routers/aiRoutes.js";
@@ -8,6 +9,7 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 const app = express();
 
 app.set("trust proxy", 1);
+app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
 app.use(express.json());
 app.use((req, res, next) => {
 	req.requestId = req.headers["x-request-id"] || randomUUID();
